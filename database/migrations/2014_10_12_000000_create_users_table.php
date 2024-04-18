465dfa1ebeae20cf0756d2e6f733cb6992f60407
Ramil Akhmetov <ramil147258369@gmail.com>
2024-04-18 18:54:34 +0700

    test gpg

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->string('password')->nullable();
             $table->text('photo')->nullable();
             $table->schemalessAttributes('extra_attributes');
-            //todo should delete remember token //test
+            //todo should delete remember token
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
